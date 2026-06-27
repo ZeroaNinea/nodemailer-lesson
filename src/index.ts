74405ecc7fa928cf5ec2c87d0dcc386c67149471
Catgirl <zeroaninea@gmail.com>
2026-06-27 14:04:48 +0400

    waiting until i'll be able to get my phone

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-console.log('Hello, World!');
-const num = 42;
+import nodemailer from 'nodemailer';
 
-console.log(num);
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  auth: {
+    user: '', // your email
+    pass: '', // the app password you generated, paste without spaces
+  },
+  secure: true,
+  port: 465,
+});
+(async () => {
+  await transporter.sendMail({
+    from: '', // your email
+    to: '', // the email address you want to send an email to
+    subject: '', // The title or subject of the email
+    html: '', // I like sending my email as html, you can send \
+    // emails as html or as plain text
+  });
+
+  console.log('Email sent');
+})();
