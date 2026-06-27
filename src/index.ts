ff07efecbb7ef5823b056e2bd91fd9c9540d781a
Catgirl <zeroaninea@gmail.com>
2026-06-27 15:35:19 +0400

    email configuration added

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,36 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
-  host: 'smtp.gmail.com',
+  // host: 'smtp.gmail.com',
   auth: {
-    user: '', // your email
-    pass: '', // the app password you generated, paste without spaces
+    user: process.env.SENDER_EMAIL || '',
+    pass: process.env.APP_PASSWORD || '',
   },
   secure: true,
   port: 465,
 });
 (async () => {
-  await transporter.sendMail({
-    from: '', // your email
-    to: '', // the email address you want to send an email to
-    subject: '', // The title or subject of the email
-    html: '', // I like sending my email as html, you can send \
-    // emails as html or as plain text
-  });
-
-  console.log('Email sent');
+  await transporter.sendMail(
+    {
+      from: process.env.SENDER_EMAIL || '',
+      to: process.env.RECEIVER_EMAIL || '',
+      subject: 'Being Silly',
+      // text: ':3',
+      html: `
+      <div style="text-align: center; font-size: 50px;">:3</div>
+    `,
+    },
+    (err, info) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(info);
+      }
+    },
+  );
 })();
